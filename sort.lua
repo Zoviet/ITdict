06f468d9fd59532a91853f43d1bf29a8e71241fa
Zoviet <alexandr@asustem.ru>
2024-10-03 00:36:50 +0400

    ver1

---
+++ sort.lua
@@ -0,0 +1,33 @@
+local output = assert(io.open('README.md', "w"))
+
+local file = io.input('ITdict.md')
+local text = io.read("*a") file:close()
+
+local file = "ITdict.md"
+local t= {}
+
+function sorted(dict)
+    local items = {}
+    for key, value in pairs(dict) do
+        table.insert(items, {key, value})    end
+
+    table.sort(items, function(a, b) return a[1] < b[1] end)
+    return items
+end
+
+for line in io.lines(file) do    
+    local term, text = string.match(line, "%*%*%*(.+)%*%*%*(.+)")
+    if term and text then
+		table.insert(t, {term, text}) 
+	end	
+end
+
+table.sort(t, function(a, b) return a[1] < b[1] end)
+
+output:write('# Словарь ИТ для начинающих\n\n')
+
+for _,val in pairs(t) do
+	output:write('***'..val[1]..'*** '..val[2]..'\n\n')	
+end
+
+output:close()
